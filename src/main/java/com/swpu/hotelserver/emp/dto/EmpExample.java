f9e034f2e405a4f8519cf6aef714e8e3fb80d8ae
东天柴“ <3396856578@qq.com>
2024-07-25 16:38:43 +0800

    员工管理

---
+++ src/main/java/com/swpu/hotelserver/emp/dto/EmpExample.java
@@ -0,0 +1,17 @@
+package com.swpu.hotelserver.emp.dto;
+
+import lombok.Data;
+
+@Data
+public class EmpExample {
+    private Integer id;
+    private String name;
+    private String username;
+    private String password;
+    private String sex;
+    private Integer age;
+    private String position;
+    private String imgUrl;
+    private String phone;
+    private String idNum;
+}
